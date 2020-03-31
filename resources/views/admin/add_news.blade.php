1a91f354e1a0ce2bfe97c1cdf87965bd7ae274b2
Anton Danilov <anton2009danilov@yandex.ru>
2020-03-31 12:50:01 +0300

    сделал форму добавления новости аккуратной, кнопку добавил

--- resources/views/admin/add_news.blade.php
+++ resources/views/admin/add_news.blade.php
@@ -10,11 +10,12 @@
 
 @section('content')
     <div class="container mt-2">
+
         <h3>Форма для добавление новости</h3>
         <form>
             <div class="form-group">
                 <label for="exampleFormControlInput1">Заголовок</label>
-                <input type="text" class="for m-control" id="exampleFormControlInput1" placeholder="Заголовок">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Заголовок">
             </div>
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Категория</label>
@@ -26,8 +27,11 @@
                 </select>
             </div>
             <div class="form-group">
-                <label for="exampleFormControlTextarea1">Example textarea</label>
+                <label for="exampleFormControlTextarea1">Текст новости</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Создать новость</button>
             </div>
         </form>
     </div>
