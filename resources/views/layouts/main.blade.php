e3a200bf9829ed67004aec178c6ca4c64efddf2f
Anton Danilov <anton2009danilov@yandex.ru>
2020-04-03 11:07:15 +0300

    перевод шаблонов auth на layout/main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -62,7 +62,7 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             @yield('content')
         </div>
     </div>
