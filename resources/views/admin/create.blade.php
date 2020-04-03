e9780be5619679880f28d0a9a2790ac6d97d5c2a
Anton Danilov <anton2009danilov@yandex.ru>
2020-04-03 15:24:42 +0300

    добавлен checkbox в форму создания новости

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,3 +1,4 @@
+<?php //$categories = []?>
 @extends('layouts.main')
 
 @section('title')
@@ -10,30 +11,41 @@
 
 @section('content')
     <div class="card mt-5">
-        <div class="card-header">Форма для добавление новости</div>
+        <div class="card-header font-weight-bold">Форма для добавление новости</div>
         <div class="card-body">
 
-{{--            <h3>Форма для добавление новости</h3>--}}
-            <form>
+            <form method="POST" action="{{ route('admin.create') }}">
+                @csrf
                 <div class="form-group">
-                    <label for="exampleFormControlInput1">Заголовок</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Заголовок">
+                    <label for="title">Заголовок</label>
+                    <input name="title" type="text" class="form-control" id="title" placeholder="Заголовок">
                 </div>
                 <div class="form-group">
-                    <label for="exampleFormControlSelect1">Категория</label>
-
-                    <select class="form-control" id="exampleFormControlSelect1">
-                        @foreach ($categories as $category)
-                            <option value="{{ $category['id'] }}"> {{ $category['name'] }} </option>
-                        @endforeach
-                    </select>
+                    <label for="category">Категория</label>
+                    @if (!empty($categories))
+                        <select name="category" class="form-control" id="category">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category['id'] }}"> {{ $category['name'] }} </option>
+                            @endforeach
+                        </select>
+                    @else
+                        <h2>Нет категорий</h2>
+                    @endif
                 </div>
                 <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Текст новости</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <label for="text">Текст новости</label>
+                    <textarea name="text" class="form-control" id="text" rows="3"></textarea>
+                </div>
+{{--                <div class="form-group">--}}
+{{--                    <label for="private">Новость доступна только зарегистрированным пользователям</label>--}}
+{{--                    <input name="private" type="checkbox" value="1" class="form-control" id="private">--}}
+{{--                </div>--}}
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Доступно только зарегистрированным пользователям</label>
                 </div>
                 <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Создать новость</button>
+                    <button type="submit" class="btn btn-outline-primary">Создать новость</button>
                 </div>
 
             </form>
