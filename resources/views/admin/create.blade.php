1f8703b9f7a6a82d32be6cf3779da50088f2a903
Anton Danilov <anton2009danilov@yandex.ru>
2020-04-03 13:26:15 +0300

    изменения стилизации формы создания новости

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -9,31 +9,36 @@
 @endsection
 
 @section('content')
-    <div class="container mt-2">
+    <div class="card mt-5">
+        <div class="card-header">Форма для добавление новости</div>
+        <div class="card-body">
 
-        <h3>Форма для добавление новости</h3>
-        <form>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Заголовок</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Заголовок">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlSelect1">Категория</label>
+{{--            <h3>Форма для добавление новости</h3>--}}
+            <form>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Заголовок</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Заголовок">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Категория</label>
 
-                <select class="form-control" id="exampleFormControlSelect1">
-                    @foreach ($categories as $category)
-                        <option value="{{ $category['id'] }}"> {{ $category['name'] }} </option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Текст новости</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-            <div class="col-sm-10">
-                <button type="submit" class="btn btn-primary">Создать новость</button>
-            </div>
-        </form>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        @foreach ($categories as $category)
+                            <option value="{{ $category['id'] }}"> {{ $category['name'] }} </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Текст новости</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Создать новость</button>
+                </div>
+
+            </form>
+
+        </div>
     </div>
 @endsection
 
