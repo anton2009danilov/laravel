044d81908573e3dffa1aef7588f2f10a21670ad9
Anton Danilov <anton2009danilov@yandex.ru>
2020-04-04 03:44:53 +0300

    Модели берут данные из файлов

--- app/News.php
+++ app/News.php
@@ -3,70 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use function MongoDB\BSON\fromJSON;
 
 class News extends Model
 {
-    private static $news = [
-
-        1 => [
-            'id' => 1,
-            'title' => 'Новость 1 (крайне интересно)',
-            'text' => 'Горячая новость №1',
-            'category_id' => 1,
-            'isPrivate' => true
-        ],
-        [
-            'id' => 2,
-            'title' => 'Новость 2',
-            'text' => 'Леденящая кровь новость 1',
-            'category_id' => 2,
-            'isPrivate' => false
-        ],
-        [
-            'id' => 3,
-            'title' => 'Новость 3',
-            'text' => 'Прогноз: всё будет хорошо',
-            'category_id' => 3,
-            'isPrivate' => false
-        ],
-        [
-            'id' => 4,
-            'title' => 'Новость 4 (крайне интересно)',
-            'text' => 'Горячая новость №2',
-            'category_id' => 1,
-            'isPrivate' => true
-        ],
-        [
-            'id' => 5,
-            'title' => 'Новость 5',
-            'text' => 'Горячая новость №3',
-            'category_id' => 1,
-            'isPrivate' => false
-        ],
-        [
-            'id' => 6,
-            'title' => 'Новость 6 (крайне интересно)',
-            'text' => 'Леденящая кровь новость 2',
-            'category_id' => 2,
-            'isPrivate' => true
-        ],
-    ];
-
     public static function getNews()
     {
-//        \File::get();
-        return static::$news;
+        $news = \Storage::get('news.json');
+        $news = json_decode($news, true);
+        return $news;
     }
 
     public static function getNewsId($id)
     {
-        return static::$news[$id];
+        return static::getNews()[$id];
+
     }
 
     public static function getNewsByCategoryName($name) {
         $id = Category::getCategoryIdByName($name);
         $news = [];
-        foreach (static::$news as $item) {
+        foreach (static::getNews() as $item) {
             if($item['category_id'] == $id) {
                 $news[] = $item;
             }
