d4ffb1d6e95a55d32363663adba54a3431f4d32d
Anton Danilov <anton2009danilov@yandex.ru>
2020-04-04 06:02:29 +0300

    http тесты

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\News;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -15,7 +16,23 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
+//        $response->assertSee('<a class="nav-link active" href="http://laravel.local">Главная</a>');
+        $response->assertSeeText('Добро пожаловать');
+        $response->assertSeeInOrder(['active', 'Главная', 'Новости']);
+        $response->assertSeeInOrder(['Главная', 'Новости', 'Категории', 'Админка']);
+
+    }
+
+    public function testNewsIndexTest()
+    {
+        $response = $this->get('/news');
+        $response->assertStatus(200);
+//        $response->assertSee('<a class="nav-link active" href="http://laravel.local">Главная</a>');
+        $response->assertSeeInOrder(['Главная', 'active', 'Новости']);
+        $response->assertSeeInOrder(['Главная', 'Новости', 'Категории', 'Админка']);
+        $response->assertViewHas('news', News::getNews());
+        $response->assertViewIs('news.index');
+
     }
 }
