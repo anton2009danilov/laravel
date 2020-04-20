cc177a9eb9893ffc6a9b573f2d08b8a36a96c141
Anton Danilov <anton2009danilov@yandex.ru>
2020-04-20 21:01:47 +0300

    Правила валидации: запрет на создание пользователей с одиниковым значение email

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
     public static function rules() {
         return [
             'name' => ['required', 'min:3', 'max:25'],
-            'email' => 'required|min:6|email',
+            'email' => 'required|min:6|email|unique:users,email',
         ];
     }
 }
