73977616ed28e87981d1f032fb4ef6b4f6c7ffff
Anton Danilov <anton2009danilov@yandex.ru>
2020-04-25 02:28:38 +0300

    Переделано представление Админка-Новости

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -15,20 +15,31 @@
 
     <div class="card mt-3">
         <div class="card-body">
-            <div class="list-group">
+            <div class="list-group m-1">
                 @forelse ($news as $item)
+                    <div class="card">
+                        <div class="border-dark p-1 row">
+                            <div class="col-sm-9">{{ $item->title }}</div>
+                            <div class="col-sm-3 d-flex align-items-center float-right">
+                                <a href="{{ route('admin.news.edit', $item) }}" class="">
+                                    <button type="button" class="btn btn-success btn-sm">
+                                        <i class="far fa-edit"></i>
+                                    </button>
+                                </a>
+                                <a href="{{ route('admin.news.destroy', $item) }}" class="">
+                                    <button type="button" class="btn btn-danger btn-sm">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </a>
 
-                    <div class="list-group-item m-2">
-                        <h3>{{ $item->title }}</h3>
-                        <a href="{{ route('admin.news.edit', $item) }}" class="float-right mr-1">
-                            <button type="button" class="btn btn-success">Edit</button>
-                        </a>
-                        <a href="{{ route('admin.news.destroy', $item) }}" class="float-right mr-1">
-                            <button type="button" class="btn btn-danger">Delete</button>
-                        </a>
-                        <a href="{{ route('news.show', $item) }}" class="float-right mr-1">
-                            <button type="button" class="btn btn-primary">Show</button>
-                        </a>
+                                <a href="{{ route('news.show', $item) }}" class="">
+                                    <button type="button" class="btn btn-primary btn-sm">
+                                        <i class="fas fa-glasses"></i>
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+
                     </div>
                 @empty
                     <h3>Нет новостей</h3>
